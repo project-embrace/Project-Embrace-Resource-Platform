fc715b7e433d80d1703aa49546387090436cdb8e
Kinkade Darling <ckinkadedarling@gmail.com>
2019-11-18 15:59:36 -0700

    Minor Model Alterations

--- ProjEmb/local_settings.py
+++ ProjEmb/local_settings.py
@@ -13,13 +13,14 @@ EMAIL_USE_TLS = True
 
 
 # Database Settings
+# postgresql-triangular-53659
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd1nvalp9fu04ti',
-        'USER': 'sawdzaccadzqbi',
-        'PASSWORD':'2bf198630fad777e0aad364fd5dbf1a60ea63628fbe263c5e8254c340d282352',
-        'HOST': 'ec2-107-21-226-44.compute-1.amazonaws.com',
+        'NAME': 'd7jibjvnj3lli7',
+        'USER': 'cbxohzajxojsss',
+        'PASSWORD':'dc7c4fcee7cc8e347b36015922e651c4b058d1525d939b8afc914c3b765f6659',
+        'HOST': 'ec2-107-20-173-2.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
